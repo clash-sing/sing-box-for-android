3ccb550818272aba68ec82f961309f6fa915c2de
flutter-lib <flutterlib@gmail.com>
2025-09-03 23:49:05 +0800

    feat(proxylib): 添加选择器和 URL 测试功能
    - 新增 OutTypeSelector 类，实现选择器功能
    - 新增 OutTypeUrlTest 类，实现 URL 测试功能
    - 两个新功能均支持序列化和 parcelable

---
+++ proxyLib/src/main/java/com/clashsing/proxylib/schema/singbox/out/OutTypeSelector.kt
@@ -0,0 +1,13 @@
+package com.clashsing.proxylib.schema.singbox.out
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
+
+@Parcelize
+@Serializable
+data class OutTypeSelector(
+    override val type: String = OutType.Type.SELECTOR,
+    override val tag: String,
+    val outbounds: List<String> = emptyList(),
+) : OutType, Parcelable
---
+++ proxyLib/src/main/java/com/clashsing/proxylib/schema/singbox/out/OutTypeUrlTest.kt
@@ -0,0 +1,18 @@
+package com.clashsing.proxylib.schema.singbox.out
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
+
+@Parcelize
+@Serializable
+data class OutTypeUrlTest(
+    override val type: String = OutType.Type.URLTEST,
+    override val tag: String,
+    val url: String = "",
+    /** 测试间隔。 默认使用 3m */
+    val interval: String = "3m",
+    /** 以毫秒为单位的测试容差。 默认使用 50 */
+    val tolerance: Int = 50,
+    val outbounds: List<String> = emptyList()
+) : OutType, Parcelable
