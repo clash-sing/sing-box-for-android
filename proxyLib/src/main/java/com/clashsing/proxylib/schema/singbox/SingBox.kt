06ca67157ae11ce0a8861b4cbf0290564728ed39
flutter-lib <flutterlib@gmail.com>
2025-09-04 10:18:35 +0800

    refactor(singbox): 重构 SingBox 数据类
    - 将 SingBoxExp 和 SingBoxLog 类嵌套到 SingBox 类中- 重命名嵌套类为 Exp 和 Log
    - 更新属性名称以适应新的嵌套结构
    - 删除单独的 SingBoxExp 和 SingBoxLog 文件

--- proxyLib/src/main/java/com/clashsing/proxylib/schema/singbox/SingBox.kt
+++ proxyLib/src/main/java/com/clashsing/proxylib/schema/singbox/SingBox.kt
@@ -4,6 +4,7 @@ import android.os.Parcelable
 import com.clashsing.proxylib.schema.singbox.dns.Dns
 import com.clashsing.proxylib.schema.singbox.out.OutType
 import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Parcelize
@@ -11,6 +12,43 @@ import kotlinx.serialization.Serializable
 data class SingBox(
     val dns: Dns,
     val outbounds: List<OutType>,
-    val experimental: SingBoxExp = SingBoxExp(),
-    val log: SingBoxLog = SingBoxLog()
-) : Parcelable
+    val experimental: Exp = Exp(),
+    val log: Log = Log()
+) : Parcelable {
+
+    @Parcelize
+    @Serializable
+    data class Log(
+        val disabled: Boolean = false,
+        val level: String = LOG_INFO,
+        val timestamp: Boolean = true,
+        val output: String? = null
+    ) : Parcelable {
+        companion object {
+            const val LOG_TRACE = "trace"
+            const val LOG_DEBUG = "debug"
+            const val LOG_INFO = "info"
+            const val LOG_WARN = "warn"
+            const val LOG_ERROR = "error"
+            const val LOG_FATAL = "fatal"
+            const val LOG_PANIC = "panic"
+
+        }
+    }
+
+    @Parcelize
+    @Serializable
+    data class Exp(
+        @SerialName("cache_file")
+        val cacheFile: CacheFile = CacheFile(),
+    ) : Parcelable {
+        @Parcelize
+        @Serializable
+        data class CacheFile(
+            val enabled: Boolean = true,
+            @SerialName("store_fakeip")
+            val storeFakeip: Boolean = true
+        ) : Parcelable
+
+    }
+}
