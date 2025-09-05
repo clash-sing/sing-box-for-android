bfe92209dfe4b5fe91d2d587e9f310d3bcc6f3ee
flutter-lib <flutterlib@gmail.com>
2025-09-05 11:22:48 +0800

    refactor(proxyLib): 为 StringOrStringList.StringValue 添加自定义序列化器
    - 明确指定 StringValue 的序列化器，以提高代码的可读性和性能
    - 实现 StringValueSerializer，用于 StringValue 的序列化和反序列化
    - 优化 StringOrStringListSerializer 中的类型检查错误信息

--- proxyLib/src/main/java/com/clashsing/proxylib/schema/StringOrStringList.kt
+++ proxyLib/src/main/java/com/clashsing/proxylib/schema/StringOrStringList.kt
@@ -1,6 +1,12 @@
 package com.clashsing.proxylib.schema
 import kotlinx.serialization.DeserializationStrategy
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonContentPolymorphicSerializer
 import kotlinx.serialization.json.JsonElement
@@ -8,19 +14,31 @@ import kotlinx.serialization.json.JsonPrimitive
 
 @Serializable
 sealed class StringOrStringList {
-    @Serializable
+    @Serializable(with = StringValueSerializer::class) // 明确指定序列化器
     data class StringValue(val value: String) : StringOrStringList()
 
     @Serializable
     data class ListValue(val value: List<String>) : StringOrStringList()
 }
 
+object StringValueSerializer : KSerializer<StringOrStringList.StringValue> {
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("StringOrStringList.StringValue", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: StringOrStringList.StringValue) {
+        encoder.encodeString(value.value)
+    }
+
+    override fun deserialize(decoder: Decoder): StringOrStringList.StringValue {
+        return StringOrStringList.StringValue(decoder.decodeString())
+    }
+}
 object StringOrStringListSerializer : JsonContentPolymorphicSerializer<StringOrStringList>(StringOrStringList::class) {
     override fun selectDeserializer(element: JsonElement): DeserializationStrategy<StringOrStringList> {
         return when (element) {
             is JsonArray -> StringOrStringList.ListValue.serializer()
             is JsonPrimitive -> StringOrStringList.StringValue.serializer()
-            else -> throw IllegalArgumentException("Unsupported type")
+            else -> throw IllegalArgumentException("Unsupported type for StringOrStringList: ${element::class}")
         }
     }
 }
