c1126143a0c580554bcf2d41a697199f849c4292
flutter-lib <flutterlib@gmail.com>
2025-09-06 17:29:37 +0800

    refactor(proxyLib): 重构 SubUserinfo 类的位置
    - 将 SubUserinfo 类从单独的文件移动到 SubUserinfoManager.kt 文件中
    - 保持了类的结构和功能不变，仅更改了其定义的位置
    - 这种重构可能有助于更好地组织代码，将相关类放在同一个文件中

--- proxyLib/src/main/java/com/clashsing/proxylib/SubUserinfoManager.kt
+++ proxyLib/src/main/java/com/clashsing/proxylib/SubUserinfoManager.kt
@@ -2,6 +2,7 @@ package com.clashsing.proxylib
 
 import com.clashsing.proxylib.schema.customJson
 import com.tencent.mmkv.MMKV
+import kotlinx.serialization.Serializable
 
 object SubUserinfoManager {
 
@@ -22,3 +23,17 @@ object SubUserinfoManager {
     }
 
 }
+
+@Serializable
+data class SubUserinfo(
+    /** 当月使用量，单位：Byte */
+    val usedBytes: Long?,
+    /** 每月限额，单位：Byte */
+    val totalBytes: Long?,
+    /** 订阅到期日 */
+    val expireTimestamp: Long?,
+    /** 代理服务提供商的网址 */
+    val spUrl: String = "",
+    /** 代理服务提供商的名称 */
+    val spDisposition: String = ""
+)
